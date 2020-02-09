2d44d9679f6e1f97d527320a7f4a63abcffa5ed5
langibryant <langibryant@gmail.com>
2020-02-08 22:34:03 -0700

    first commit

--- game/DEFINITIONS.hpp
+++ game/DEFINITIONS.hpp
@@ -1,4 +1,4 @@
-#define SCREEN_WIDTH 1920 / 2
-#define SCREEN_HEIGHT 1080 / 2
-#define SCALE_X 1
-#define SCALE_Y 1
+#define SCREEN_WIDTH 1920 / 12
+#define SCREEN_HEIGHT 1080 / 12
+#define SCALE_X 6
+#define SCALE_Y 6
--- game/game.hpp
+++ game/game.hpp
@@ -8,6 +8,7 @@ public:
     sAppName = "temp";
   }
 
+
   bool OnUserCreate() override {
 
     return true;
@@ -16,10 +17,49 @@ public:
   bool OnUserUpdate(float fElapsedTime) override {
     Clear(olc::BLACK);
 
+    // std::cout << GetMouseX() << " " << GetMouseY() << '\n';
+
+    int mouseX = GetMouseX();
+    int mouseY = GetMouseY();
+
+
+    for(int y = 0; y < map.size(); y++){
+      for(int x = 0; x < map.at(y).size(); x++){
+        if(map.at(y).at(x) == 1){
+
+          int newx = (xOffset + (x*16)) - ((xOffset + (x*16)))/2;
+          int newy = (yOffset + (y*16)) - ((yOffset + (y*16)))/2;
+          DrawRect(xOffset + (x*16), yOffset + (y*16), 16, 16);
+          // std::cout << xOffset + (x*16) << " " << yOffset + (y*16) << '\n';
+
+        }
+      }
+    }
+
+    int mapX = (map.size() * 16) - (map.size() * 16);
+    int mapY = (map.size() * 16) - (map.size() * 16);
+
+
+    if(GetMouse(0).bHeld){
+      std::cout << xOffset << " " << yOffset << '\n';
+      xOffset = mouseX;
+      yOffset = mouseY;
+    }
+
     return true;
   }
 
 
 private:
+  std::vector<std::vector<int>> map = {
+    {0, 0, 0, 1, 0 ,0, 0},
+    {0, 1, 1, 0, 1 ,1, 0},
+    {0, 1, 0, 0, 0 ,0, 1},
+    {0, 0, 1, 0, 0 ,1, 0},
+    {0, 0, 1, 1, 1 ,1, 0},
+  };
+
+  int xOffset = 0;
+  int yOffset = 0;
 
 };
